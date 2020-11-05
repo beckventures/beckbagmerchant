e90e7b6bc1275bec6a67a9ebcc5bcf601aea19e6
contactbeckfriends <contactbeckfriends@gmail.com>
2020-11-05 17:30:23 +0530

    Updates

---
+++ precache-manifest.43b8e64855f161ad7353d643b5790f62.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bfe04728b9a5d3e3d74a1fa23a7f16fc",
+    "url": "/beckbagmerchant/index.html"
+  },
+  {
+    "revision": "120b155d9ccbbdb4f24d",
+    "url": "/beckbagmerchant/static/css/2.fe52bc77.chunk.css"
+  },
+  {
+    "revision": "c0a24457b4848420114c",
+    "url": "/beckbagmerchant/static/css/main.ada56379.chunk.css"
+  },
+  {
+    "revision": "120b155d9ccbbdb4f24d",
+    "url": "/beckbagmerchant/static/js/2.ef02655a.chunk.js"
+  },
+  {
+    "revision": "b8cbd6dd4796ea24ef0b5cd49abd8258",
+    "url": "/beckbagmerchant/static/js/2.ef02655a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "88a0d4215723d33bc0f7",
+    "url": "/beckbagmerchant/static/js/3.e9e8715d.chunk.js"
+  },
+  {
+    "revision": "7ec01595672f75e83fd81b41f132f4c1",
+    "url": "/beckbagmerchant/static/js/3.e9e8715d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a0dcd3693e29132524f",
+    "url": "/beckbagmerchant/static/js/4.8a2bb1ae.chunk.js"
+  },
+  {
+    "revision": "0512cf1466087f5c44c5c61e4b32a001",
+    "url": "/beckbagmerchant/static/js/4.8a2bb1ae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b2aee7c7848093e0e548",
+    "url": "/beckbagmerchant/static/js/5.7dd1a8ce.chunk.js"
+  },
+  {
+    "revision": "3a7bc1760b9d274e2e5e7531bce86076",
+    "url": "/beckbagmerchant/static/js/5.7dd1a8ce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c0a24457b4848420114c",
+    "url": "/beckbagmerchant/static/js/main.f9cb1fba.chunk.js"
+  },
+  {
+    "revision": "dc192b57bfed2afd6be1ea5558f17255",
+    "url": "/beckbagmerchant/static/js/main.f9cb1fba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6e4f6f303ddaa037bd0a",
+    "url": "/beckbagmerchant/static/js/runtime-main.1fa3be29.js"
+  },
+  {
+    "revision": "bd6820f1a4015e9645e470ea8e7b1501",
+    "url": "/beckbagmerchant/static/media/anime3.bd6820f1.png"
+  },
+  {
+    "revision": "03ef1918e505c3e3471f9369ef7a638f",
+    "url": "/beckbagmerchant/static/media/nucleo.03ef1918.eot"
+  },
+  {
+    "revision": "5987dd12fea78ce5f97ae601b08ec03c",
+    "url": "/beckbagmerchant/static/media/nucleo.5987dd12.woff2"
+  },
+  {
+    "revision": "b17a118e13e53558658b681a0ebdad82",
+    "url": "/beckbagmerchant/static/media/nucleo.b17a118e.ttf"
+  },
+  {
+    "revision": "f0b489a5dbbff08833d21024f9fcbd4e",
+    "url": "/beckbagmerchant/static/media/nucleo.f0b489a5.woff"
+  },
+  {
+    "revision": "eb6be414b75c412c13eeee2d2e8390cb",
+    "url": "/beckbagmerchant/static/media/react-logo.eb6be414.png"
+  }
+]);
