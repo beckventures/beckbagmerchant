28e4abcfafa261ee368321c21a819df7b04aca63
contactbeckfriends <contactbeckfriends@gmail.com>
2020-11-10 02:38:13 +0530

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/beckbagmerchant/precache-manifest.6ef6be57f6fefda28ddca703b3296ce9.js"
+  "/beckbagmerchant/precache-manifest.a5c825f7abcda037abd29c68aae7337f.js"
 );
 
 self.addEventListener('message', (event) => {
