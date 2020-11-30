ba39467378bd755ba03a8516d103ea248d9d692d
contactbeckfriends <contactbeckfriends@gmail.com>
2020-11-30 16:47:02 +0530

    Updates

---
+++ precache-manifest.623c36a683c91833be5206358be00f47.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72117b1eca1bef7bdf814bb2b9a13afa",
+    "url": "/beckbagmerchant/index.html"
+  },
+  {
+    "revision": "821b1126eb1a532b7c11",
+    "url": "/beckbagmerchant/static/css/2.65c2f4bc.chunk.css"
+  },
+  {
+    "revision": "94572f911d00e83ae3b1",
+    "url": "/beckbagmerchant/static/css/main.d72bcf5d.chunk.css"
+  },
+  {
+    "revision": "821b1126eb1a532b7c11",
+    "url": "/beckbagmerchant/static/js/2.4f14fb2b.chunk.js"
+  },
+  {
+    "revision": "b8cbd6dd4796ea24ef0b5cd49abd8258",
+    "url": "/beckbagmerchant/static/js/2.4f14fb2b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8f87da427e19ffb3665e",
+    "url": "/beckbagmerchant/static/js/3.955d4963.chunk.js"
+  },
+  {
+    "revision": "7ec01595672f75e83fd81b41f132f4c1",
+    "url": "/beckbagmerchant/static/js/3.955d4963.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "03f5b2c8b2092a9fb4cf",
+    "url": "/beckbagmerchant/static/js/4.0d70f5b8.chunk.js"
+  },
+  {
+    "revision": "0512cf1466087f5c44c5c61e4b32a001",
+    "url": "/beckbagmerchant/static/js/4.0d70f5b8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8bbaf7c79e5e0d15e97b",
+    "url": "/beckbagmerchant/static/js/5.ef0f1bef.chunk.js"
+  },
+  {
+    "revision": "3a7bc1760b9d274e2e5e7531bce86076",
+    "url": "/beckbagmerchant/static/js/5.ef0f1bef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "94572f911d00e83ae3b1",
+    "url": "/beckbagmerchant/static/js/main.1d2b40f2.chunk.js"
+  },
+  {
+    "revision": "dc192b57bfed2afd6be1ea5558f17255",
+    "url": "/beckbagmerchant/static/js/main.1d2b40f2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c652ae61c73697f5f001",
+    "url": "/beckbagmerchant/static/js/runtime-main.3f570e12.js"
+  },
+  {
+    "revision": "1cedb6e919bfed6a2c1ec00b5d8ee620",
+    "url": "/beckbagmerchant/static/media/UploadIcon.1cedb6e9.svg"
+  },
+  {
+    "revision": "bd6820f1a4015e9645e470ea8e7b1501",
+    "url": "/beckbagmerchant/static/media/anime3.bd6820f1.png"
+  },
+  {
+    "revision": "03ef1918e505c3e3471f9369ef7a638f",
+    "url": "/beckbagmerchant/static/media/nucleo.03ef1918.eot"
+  },
+  {
+    "revision": "5987dd12fea78ce5f97ae601b08ec03c",
+    "url": "/beckbagmerchant/static/media/nucleo.5987dd12.woff2"
+  },
+  {
+    "revision": "b17a118e13e53558658b681a0ebdad82",
+    "url": "/beckbagmerchant/static/media/nucleo.b17a118e.ttf"
+  },
+  {
+    "revision": "f0b489a5dbbff08833d21024f9fcbd4e",
+    "url": "/beckbagmerchant/static/media/nucleo.f0b489a5.woff"
+  },
+  {
+    "revision": "eb6be414b75c412c13eeee2d2e8390cb",
+    "url": "/beckbagmerchant/static/media/react-logo.eb6be414.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/beckbagmerchant/precache-manifest.5513d8ee44a162023b6c88ba82b41428.js"
+  "/beckbagmerchant/precache-manifest.623c36a683c91833be5206358be00f47.js"
 );
 
 self.addEventListener('message', (event) => {
